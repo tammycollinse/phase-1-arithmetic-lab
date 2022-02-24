7d9f7ea6d9c8917b6c24da3645a0a762ebbeee7f
Tammy <tammycollinse@gmail.com>
2022-02-24 07:45:17 -0500

    commit tree 681bb9e0761f1230458df2e2b8afc9dbad3e4943

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+//function multiply() {
+let num1 = 31 ;
+let num2 = 2 ;
+let multiply = num1 * num2; 
+//console.log(multiply);
+//}
+
+
+//function random(random){
+let random =  Math.floor(Math.random() * 10);  
+//}
+
+//function mod(mod){
+let num3 = 44 ;
+let  num4 = 10 ;
+let mod = num3 % num4 ;
+//}
+
+//function max(max){
+let max = Math.max(1,2,4,20,8) ;   
+//}
